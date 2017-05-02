11670917bee6964d921dacdd0b8921f03140a56b
actors315 <actors315@gmail.com>
2017-05-02 09:16:52 +0800

    '抽奖类改为虚类，需实现后方可使用'

--- src/Lottery.php
+++ src/Lottery.php
@@ -8,16 +8,16 @@
 
 namespace lingyin\lottery;
 
-
-class Lottery
+/**
+ * Class Lottery
+ * @package lingyin\lottery
+ *
+ * 抽奖核心类
+ *
+ * 只是实现抽奖动作，不包含前置较验，不包含抽后处理
+ */
+abstract class Lottery
 {
-    /**
-     * 抽奖标识
-     *
-     * @var string
-     */
-    private $lottery_id;
-
     /**
      * 奖品信息列表
      *
@@ -32,10 +32,9 @@ class Lottery
      */
     private $default_prize;
 
-    public function __construct($lottery_id,$prize_list = [])
+    public function __construct($prize_list = [])
     {
-        $this->lottery_id = $lottery_id;
-        if(!empty($prize_list)){
+        if (!empty($prize_list)) {
             $this->setPrizeList($prize_list);
         }
     }
@@ -46,35 +45,66 @@ class Lottery
      * @param $prize_list array
      *      格式如下
      *      [
-     *          ['id' => 1, 'name' => 'FREE GIFT', 'num' => 6, 'probabilities' => 0.325000, 'is_default' => 0]
+     *          ['index' => 1, 'name' => 'FREE GIFT', 'num' => 6, 'probabilities' => 0.325000, 'is_default' => 0]
      *      ]
      */
-    public function setPrizeList($prize_list){
-        foreach ($prize_list as $prize){
-            $this->prize_list[$prize['id']] = $prize;
-            if($prize['is_default'] == 1){
+    public function setPrizeList($prize_list)
+    {
+        foreach ($prize_list as $prize) {
+            $this->prize_list[$prize['index']] = $prize;
+            if ($prize['is_default'] == 1) {
                 $this->default_prize = $prize;
             }
         }
     }
 
-    public function runLottery(){
-        $data = ['code'=>0,'msg'=>'success'];
+    public function runLottery()
+    {
+        $data = ['code' => 0, 'msg' => 'success'];
+        if ($check = $this->beforeLottery() !== true) {
+            return $check;
+        }
 
-        if(empty($this->prize_list)){
-            $data = ['code'=>1,'msg'=>'please set prize first.'];
+        if (empty($this->prize_list)) {
+            $data = ['code' => 1, 'msg' => 'please set prize first.'];
             return $data;
         }
 
-        $prize_probabilities = $this->getProbabilities($this->prize_list);
+        $prize_probabilities = $this->getProbabilities();
         $prize = $this->getRandProbabilities($prize_probabilities);
-        if($prize == 0 && !empty($this->default_prize)){
+        if ($prize == 0 && !empty($this->default_prize)) {
+            $data['msg'] = '未中奖，取默认奖品' . $this->default_prize['index'];
             $prize = $this->default_prize['id'];
         }
 
         $data['prize'] = $prize;
+        if ($check = $this->afterLottery($data) !== true) {
+            return $check;
+        }
 
         return $data;
+    }
+
+    /**
+     * 抽奖前较验，根据需要可实现该方法
+     *
+     * @return mixed
+     */
+    public function beforeLottery()
+    {
+        return true;
+    }
+
+    /**
+     * 抽奖后较验，根据需要可实现该方法
+     *
+     * @param $data
+     * @return mixed
+     */
+    public function afterLottery($data)
+    {
+
+        return true;
     }
 
     /**
@@ -106,14 +136,12 @@ class Lottery
 
     /**
      * 提取所有相应的概率
-     * @param  array $lotteryList 奖品信息
      * @return array     ["奖品keys"=>"概率值","coupons_100"=>"0"]
      */
-    private function getProbabilities($lotteryList)
+    private function getProbabilities()
     {
-        if (empty($lotteryList) || !is_array($lotteryList)) return [];
-
-        return array_column($lotteryList, "probabilities", "id");
+        if (empty($this->prize_list) || !is_array($this->prize_list)) return [];
+        return array_column($this->prize_list, "probabilities", "index");
     }
 
 }
